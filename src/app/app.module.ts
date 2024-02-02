39f2ee1803e6a0e0ad266fccfe0b9edda91843a2
Lars Mensching <133211689+Imiphon@users.noreply.github.com>
2024-02-02 09:59:06 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NoteListComponent } from './note-list/note-list.component';
+import { NoteComponent } from './note-list/note/note.component';
+import { FormsModule } from '@angular/forms';
+import { AddNoteDialogComponent } from './add-note-dialog/add-note-dialog.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { firebaseConfig } from './../environments/environment';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NoteListComponent,
+    NoteComponent,
+    AddNoteDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    //instat of firebaseConfig directly adress is possible
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+// directly environment instat of full adress: {"projectId":"danotes-a93f2","appId":"1:190961739929:web:2d080fac5830e59cefb008","storageBucket":"danotes-a93f2.appspot.com","apiKey":"AIzaSyDgzEeL_C3MnyWvRAt1wdnRfWyC220q1u8","authDomain":"danotes-a93f2.firebaseapp.com","messagingSenderId":"190961739929"}
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDgzEeL_C3MnyWvRAt1wdnRfWyC220q1u8",
+  authDomain: "danotes-a93f2.firebaseapp.com",
+  projectId: "danotes-a93f2",
+  storageBucket: "danotes-a93f2.appspot.com",
+  messagingSenderId: "190961739929",
+  appId: "1:190961739929:web:2d080fac5830e59cefb008"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig); //don't needed with @angulare/fire
+
+
